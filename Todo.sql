767550d71acc0095ba11b3740ab284b27d2b9724
akosy <develllopeurweb@gmail.com>
2023-01-24 15:26:44 +0100

    Modifications des exo todo et glacier
    je l'ai es inverser

--- Todo.sql
+++ Todo.sql
@@ -1,5 +1,4 @@
--- Todo # Exo -BDD - Analyse Merise - Projet Glacier
+-- # Exo -BDD - Analyse Merise - Projet Todo
 -- Le but de cet exercice est de maîtriser l'analyse de la création d'une base de données (BDD).
 -- ## Cahier des charges
-
 
--- glacier.sql
+++ glacier.sql
@@ -1,5 +1,3 @@
-
-
--- Glacier # Exo -BDD - Analyse Merise - Projet Todo
+-- # Exo -BDD - Analyse Merise - Projet Glacier
 -- Le but de cet exercice est de maîtriser l'analyse de la création d'une base de données (BDD).
 -- ## Cahier des charges
